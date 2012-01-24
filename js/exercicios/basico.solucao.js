52d0606e8d06e9bee4a39d09718e0d6d183e8618
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-01-24 10:33:04 -0300

    Solução do exercício básico

---
+++ js/exercicios/basico.solucao.js
@@ -0,0 +1,42 @@
+// q1 : Dê console.log() nos valores que são avaliados para false
+
+var q1Var1 = "hello",
+    q1Var2 = 0,
+    q1Var3 = true,
+    q1Var4 = "false",
+    q1Var5 = -1,
+    q1Var6 = undefined,
+    q1Var7 = null,
+    q1Var8;
+
+// BEGIN Question 1 Answer
+
+console.log(q1Var2);
+console.log(q1Var6);
+console.log(q1Var7);
+console.log(q1Var8);
+
+// END Question 1 Answer
+
+// q2: Concatene as strings abaixo e imprima no console:
+var q3Var1 = "hello, ",
+    q3Var2 = "is it me you're looking for?";
+
+// BEGIN Question 3 Answer
+
+console.log(q3Var1 + q3Var2);
+
+
+// END Question 3 Answer
+
+
+// q3: Transforme a primeira variável em um Number, some os dois valores e imprima no console.
+var q4Var1 = "24",
+    q4Var2 = 18;
+
+// BEGIN Question 4 Answer
+
+var num = parseInt(q4Var1, 10);
+console.log(q4Var2 + num);
+
+// END Question 4 Answer
