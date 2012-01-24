3c839bfc8c3bb7c17ab407fa55805295dd1cf378
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-01-24 10:55:10 -0300

    Exercicio fizzbuzz

---
+++ js/exercicios/fizzbuzz.js
@@ -0,0 +1,38 @@
+/*
+ O programa precisa pedir um número maior ou igual a 1. Todos os números entre
+ 1 e o número informado deverão ser impressos seguindo a seguinte regra:
+   Se um número for divisivel por 3, ao invés de imprimir o número, imprima
+   "Fizz".
+   Se o número for divisivel por 3, imprima "Fizz".
+   Se o número for divisivel por 5, imprima "Buzz".
+   Se o número for divisivel por 5 e 3 imprima "FizzBuzz".
+   Nos demais casos imprima apenas o número.
+  Por exemplo:
+   Se informarmos o número 20, o programa deverá imprimir:
+   1
+   2
+   Fizz
+   4
+   Buzz
+   Fizz
+   7
+   8
+   Fizz
+   Buzz
+   11
+   Fizz
+   13
+   14
+   FizzBuzz
+   16
+   17
+   Fizz
+   19
+   Buzz
+
+   referência: http://codingdojo.org/cgi-bin/wiki.pl?KataFizzBuzz
+
+   Ajuda:
+     Escrever no documento: document.write("string" + "<br>");
+     Operação de módulo: (n % 5 === 0) n é div. por 5
+*/
